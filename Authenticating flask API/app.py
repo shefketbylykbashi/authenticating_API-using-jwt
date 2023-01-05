e3ea9ad7a273e144e72df394fb03768775b1cfe9
Shefketbylykbashi <shefketbylykbashi11@gmail.com>
2023-01-06 00:37:30 +0100

    aplikimi i dekoratorit per mbrojetjen e endpoints

---
+++ Authenticating flask API/app.py
@@ -0,0 +1,31 @@
+#Detyra : Zhvillimi i nje API me disa funksione bazike,duke implementuar authentifikimin bazik ose me token
+
+from flask import Flask, request, jsonify, make_response, request, render_template, session, flash,redirect,url_for
+import jwt
+from functools import wraps
+import codecs
+
+app = Flask(__name__)
+
+
+app.config['SECRET_KEY'] = 'b0773f1b9007e927b3584b3d757a79d082da2cef98a3478eba982c2b9b4bf3de'
+
+def token_required(func):
+    @wraps(func)
+    def decorated(*args, **kwargs):
+        token = request.args.get('token')
+        print(token)
+        if not token:
+            return jsonify({'Alert!': 'Token is missing!'}), 401
+
+        try:
+
+            data = jwt.decode(token, app.config['SECRET_KEY'],algorithms=['HS256'])
+            
+        except:
+            return jsonify({'Message': 'Invalid token'}), 403
+        return func(*args, **kwargs)
+    return decorated
+
+if __name__ == "__main__":
+    app.run(debug=True)
