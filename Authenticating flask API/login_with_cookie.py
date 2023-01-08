0d0bbefccd8803a1365770cbc8916a4a550d7e0b
Shefketbylykbashi <shefketbylykbashi11@gmail.com>
2023-01-08 14:50:31 +0100

    Rregullimi i casjes permes cookie me ndihmen e jwt

--- Authenticating flask API/login_with_cookie.py
+++ Authenticating flask API/login_with_cookie.py
@@ -5,7 +5,9 @@ import jwt
 from datetime import datetime, timedelta
 from functools import wraps
 import codecs
+import http.cookies
 
+cookie = http.cookies.SimpleCookie()
 app = Flask(__name__)
 
 
@@ -14,15 +16,19 @@ app.config['SECRET_KEY'] = 'b0773f1b9007e927b3584b3d757a79d082da2cef98a3478eba98
 def token_required(func):
     @wraps(func)
     def decorated(*args, **kwargs):
-        token = request.args.get('token')
-        print(token)
+        try:
+            token = session['logged_in']
+        except:
+            return jsonify({'Alert!': 'Token is missing!'}), 401
+
         if not token:
             return jsonify({'Alert!': 'Token is missing!'}), 401
 
         try:
 
             data = jwt.decode(token, app.config['SECRET_KEY'],algorithms=['HS256'])
-            
+            global uuu
+            uuu = data["user"]
         except:
             return jsonify({'Message': 'Invalid token'}), 403
         return func(*args, **kwargs)
@@ -51,26 +57,12 @@ def public():
 def auth():
     return 'JWT is verified!'
 
-@app.route('/protected', methods=['GET'])
-def protected():
-    # Require authentication
-    auth_header = request.headers.get('Authorization')
-    if auth_header:
-        try:
-            
-            payload = jwt.decode(auth_header, app.config['SECRET_KEY'], algorithms='HS256')
-            return render_template('dashboard.html')
-        except jwt.exceptions.InvalidTokenError:
-            return 'Invalid JWT', 401
-    else:
-        return 'Authentication required', 401
-
 
 @app.route('/login', methods=['GET','POST'])
 #qeky funksion thirret saher tbahet qekjo kerkesa
 def login():
-    if request.form['username'] and request.form['password'] == '123456':
-        session['logged_in'] = True
+    if request.form['username'] and request.form['password'] == '12345678910':
+ 
         
         token = jwt.encode({
             'user': request.form['username'],
@@ -79,16 +71,11 @@ def login():
             
         },
             app.config['SECRET_KEY'])
+        session['logged_in'] = token
         return jsonify({'token': token})
     else:
         return make_response('Unable to verify', 403, {'WWW-Authenticate': 'Basic realm: "Authentication Failed "'})
         
-@app.route('/login_token', methods=['GET','POST'])
-@token_required
-def login_token():
-    session['logged_in'] = True
-
-    return render_template('dashboard.html')   
 
 @app.route('/logout', methods=['GET','POST'])
 def logout():
@@ -112,6 +99,8 @@ def refresh():
                 
          },
             app.config['SECRET_KEY'])
+    session.clear()
+    session['logged_in'] = token
     return jsonify({'token': token})
 
 if __name__ == "__main__":
